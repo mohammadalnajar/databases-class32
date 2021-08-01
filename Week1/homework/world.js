79e49934dc226d261335a5e697347b7905e2176d
Mohammad <malnajjar16@gmail.com>
2021-08-01 15:39:37 +0200

    feedback implemented

--- Week1/homework/world.js
+++ Week1/homework/world.js
@@ -8,16 +8,28 @@ const connection = mysql.createConnection({
   multipleStatements: true,
 });
 const queries = [
-  "SELECT * FROM country WHERE population > 8000000 ORDER BY continent;",
-  "SELECT * FROM country WHERE name LIKE '%land%';",
-  "SELECT * FROM city WHERE population BETWEEN 500000 AND 1000000 ORDER BY population;",
-  "SELECT * FROM country WHERE continent= 'europe';",
-  "SELECT * from country ORDER BY surfacearea desc;",
-  "SELECT * FROM city WHERE countrycode = 'NLD';",
-  "SELECT name, population FROM city WHERE countrycode = 'NLD' AND name='Rotterdam';",
-  "SELECT * FROM country ORDER BY surfacearea desc LIMIT 10;",
-  "SELECT * FROM city ORDER BY population desc LIMIT 10;",
-  "SELECT SUM(population) AS 'total population' FROM country;",
+  `SELECT * FROM country 
+  WHERE population > 8000000 
+  ORDER BY continent;`,
+  `SELECT * FROM country 
+  WHERE name LIKE '%land%';`,
+  `SELECT * FROM city 
+  WHERE population 
+  BETWEEN 500000 AND 1000000 
+  ORDER BY population;`,
+  `SELECT * FROM country 
+  WHERE continent= 'europe';`,
+  `SELECT * from country 
+  ORDER BY surfacearea desc;`,
+  `SELECT * FROM city 
+  WHERE countrycode = 'NLD';`,
+  `SELECT name, population FROM city 
+  WHERE countrycode = 'NLD' AND name='Rotterdam';`,
+  `SELECT * FROM country 
+  ORDER BY surfacearea desc LIMIT 10;`,
+  `SELECT * FROM city 
+  ORDER BY population desc LIMIT 10;`,
+  `SELECT SUM(population) AS 'total population' FROM country;`,
 ];
 connection.connect();
 
